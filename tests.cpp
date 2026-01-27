d32036ffa932fcf0eaf40584d60bdd4d28754fbf
Ethan McTague <ethan@tague.me>
2026-01-27 15:46:53 -0500

    Update assertions for test

--- tests.cpp
+++ tests.cpp
@@ -6,27 +6,30 @@ TEST_CASE("Tree", "[tests]") {
     enum class cols { parent, value, sum_with_parents };
     soa_vector<cols, int, int, int> tree;
 
+    REQUIRE(tree.empty());
+
     tree.emplace_back(0, 2, 0);
+    REQUIRE(!tree.empty());
+    REQUIRE(tree.size() == 1);
+
     tree.emplace_back(0, 6, 0);
+    REQUIRE(tree.size() == 2);
+    REQUIRE(tree.at<cols::parent>(0) == 0);
+    REQUIRE(tree.at<cols::parent>(1) == 0);
+    REQUIRE(tree.at<cols::value>(0) == 2);
+    REQUIRE(tree.at<cols::value>(1) == 6);
+    REQUIRE(tree.at<cols::sum_with_parents>(0) == 0);
+    REQUIRE(tree.at<cols::sum_with_parents>(1) == 0);
 
     tree.at<cols::sum_with_parents>(0) = tree.at<cols::value>(0);
 
     for (size_t i = 1; i < tree.size(); ++i) {
-        REQUIRE(tree.size() == 2);
-
-        std::cerr << "ITER" << std::endl;
-
         const auto parent = tree.at<cols::parent>(i);
         const auto parent_sum = tree.at<cols::sum_with_parents>(parent);
-
         const auto value = tree.at<cols::value>(i) + parent_sum;
         tree.at<cols::sum_with_parents>(i) = value;
     }
 
-    std::cerr << "DONE ITER" << std::endl;
-
     REQUIRE(tree.at<cols::sum_with_parents>(0) == 2);
     REQUIRE(tree.at<cols::sum_with_parents>(1) == 8);
-
-    std::cerr << "yayyyy" << std::endl;
 }
